fe58409f5edc3feb93cf4a943d3941e9f2ec5fdb
francisBae <qowhdgn@gmail.com>
2021-09-07 00:01:16 +0900

    commit 2021-09-07-00:01 - main 주석 추가

--- main.py
+++ main.py
@@ -7,64 +7,31 @@ from Mail import MailController as mc
 from Keyword import CompanyController as cc, InterestController as ic, KeywordController as kc
 
 def main():
+    # 구독자 리스트 조회
     subscr_list = ssc.getSubscriberList()
+
+    # 구독자 중 이메일 수신 동의자를 리스트에 추가
     email_targets = ssc.getEmailTargets(subscr_list)
-    print(email_targets)
+
+    # 이메일 제목 설정 (오늘의 기사 & 시간)
     title = mc.getCurTimeNewsMailTitle()
 
-    #키워드 리스트
-    companyList = cc.getCompanyList()
+    # 키워드 리스트 생성 (db 조회)
+    companyList = cc.getCompanyList() #기업 리스트
     keyword_1 = kc.getKeywordList(companyList,"company_name")
-
-    interestList = ic.getInterestList()
+    interestList = ic.getInterestList() #관심사 리스트
     keyword_2 = kc.getKeywordList(interestList,"interest_name")
-
     # keyword_1 = ['삼성전자', '카카오', '네이버', 'SK', '현대', 'LG', '애플']
     # keyword_2 = ['증권', '주식', '친환경','전기차','수소차','IT']
 
-
+    #네이버 뉴스 API 호출
     newsDict = nnc.keyword_combined(keyword_1,keyword_2)
 
+    #이메일용으로 내용 정제
     contents = mc.getRefinedNewsContentsForHtml(newsDict)
-    print(contents)
 
-
-
-    # cnt = 1
-    # contents = ""
-    #
-    # for key in newsDict:
-    #     contents+="<br><div><p><font size=\"4px\" face=\"맑은고딕\"><b>"+str(cnt)+". "+key+"</b></p></div>"
-    #     print(key) #삼성전자
-    #     cnt+=1
-    #     # print(newsDict[key]) #제목+링크
-    #     # contents += "<div><font size=\"3px\" face=\"돋움\">"
-    #     subcnt = 1
-    #     for title_key in newsDict[key]:
-    #
-    #         contents+="<a href="+newsDict[key][title_key]+">"+str(subcnt)+") "+title_key +"</a><br>"
-    #
-    #         # contents+=str(subcnt)+") "+title_key + " : " + newsDict[key][title_key]+"<br>"
-    #         subcnt+=1
-    #
-    #         print(title_key+" : "+newsDict[key][title_key]) #제목:링크
-    #     contents+"</font><br><br><br></div>"
-    #     print()
-
-
-    # newsDict - titleLink - key/val
-
-    # print(contents)
-
-    #HTML 정제
-
-    # contents = ms.refineHtml(contents)
-    # print(contents)
-
-    #발송 모듈
+    #발송 모듈 호출
     ms.sendEmail(email_targets, title, contents)
-
-
 
 if __name__ == "__main__":
 	main()
